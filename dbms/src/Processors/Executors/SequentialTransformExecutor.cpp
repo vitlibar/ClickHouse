ac1fedac0269158e96ed24ff78d11844b4c244a1
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2018-10-22 19:14:13 +0300

    Implement IFunctionBase::execute using processors.

--- dbms/src/Processors/Executors/SequentialTransformExecutor.cpp
+++ dbms/src/Processors/Executors/SequentialTransformExecutor.cpp
@@ -1,32 +1,76 @@
 #include <Processors/Executors/SequentialTransformExecutor.h>
+#include <Processors/ISink.h>
+#include <Processors/ISource.h>
 
 namespace DB
 {
 
+namespace
+{
+
+class SequentialTransformSource : public ISource
+{
+public:
+    explicit SequentialTransformSource(Block header) : ISource(std::move(header)) {}
+
+    Block generate() override { return std::move(input_block); }
+
+    Block input_block;
+};
+
+class SequentialTransformSink : public ISink
+{
+public:
+    explicit SequentialTransformSink(Block header) : ISink(std::move(header)) {}
+
+    void consume(Block block) override { output_block = std::move(block); }
+
+    Block output_block;
+};
+
+}
+
+SequentialTransformExecutor::SequentialTransformExecutor(Processors processors, InputPort & input, OutputPort & output)
+{
+    auto source = std::make_shared<SequentialTransformSource>(input.getHeader());
+    auto sink = std::make_shared<SequentialTransformSink>(output.getHeader());
+
+    connect(source->getOutputs().front(), input);
+    connect(output, sink->getInputs().front());
+
+    input_block = &source->input_block;
+    output_block = &sink->output_block;
+
+    processors.emplace_back(std::move(source));
+    processors.emplace_back(std::move(sink));
+
+    executor = std::make_shared<SequentialPipelineExecutor>(processors);
+}
+
 void SequentialTransformExecutor::execute(Block & block)
 {
-    output.push(std::move(block));
-    input.setNeeded();
+    *input_block = std::move(block);
+    output_block->clear();
 
-    auto status = executor.prepare();
-    while (status != IProcessor::Status::NeedData)
+    auto status = executor->prepare();
+    while (!(*output_block))
     {
         if (status == IProcessor::Status::Ready)
-            executor.work();
+            executor->work();
         else if (status == IProcessor::Status::Async)
         {
             EventCounter watch;
-            executor.schedule(watch);
+            executor->schedule(watch);
             watch.wait();
         }
         else
             throw Exception("Unexpected status for SequentialTransformExecutor: " + std::to_string(int(status)),
                             ErrorCodes::LOGICAL_ERROR);
 
-        status = executor.prepare();
+        status = executor->prepare();
     }
 
-    block = input.pull();
+    block = std::move(*output_block);
 }
 
 }
--- dbms/src/Processors/Executors/SequentialTransformExecutor.h
+++ dbms/src/Processors/Executors/SequentialTransformExecutor.h
@@ -6,32 +6,17 @@
 namespace DB
 {
 
-class SequentialTransformExecutor : public IProcessor
+class SequentialTransformExecutor
 {
-private:
-
 
 public:
-    SequentialTransformExecutor(const Processors & processors, InputPort & pipeline_input, OutputPort & pipeline_output)
-        : IProcessor({pipeline_output.getHeader()}, {pipeline_input.getHeader()})
-        , executor(processors)
-        , input(inputs.front())
-        , output(outputs.front())
-        , pipeline_input(pipeline_input)
-        , pipeline_output(pipeline_output)
-    {
-        connect(output, pipeline_input);
-        connect(pipeline_output, input);
-    }
-
+    SequentialTransformExecutor(Processors processors, InputPort & input, OutputPort & output);
     void execute(Block & block);
 
 private:
-    SequentialPipelineExecutor executor;
-    InputPort & input;
-    OutputPort & output;
-    InputPort & pipeline_input;
-    OutputPort & pipeline_output;
+    ProcessorPtr executor;
+    Block * input_block;
+    Block * output_block;
 };
 
 using SequentialTransformExecutorPtr = std::shared_ptr<SequentialTransformExecutor>;
