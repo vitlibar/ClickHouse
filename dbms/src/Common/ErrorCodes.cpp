43bb464dd0d5bff0d8fd427fe55a9aa346983209
Vitaly Baranov <vitbar@yandex-team.ru>
2019-11-04 22:17:27 +0300

    Rework Quota based on IAccessEntity.

--- dbms/src/Common/ErrorCodes.cpp
+++ dbms/src/Common/ErrorCodes.cpp
@@ -469,6 +469,7 @@ namespace ErrorCodes
     extern const int ACCESS_ENTITY_ALREADY_EXISTS = 492;
     extern const int ACCESS_ENTITY_FOUND_DUPLICATES = 493;
     extern const int ACCESS_ENTITY_STORAGE_READONLY = 494;
+    extern const int QUOTA_REQUIRES_CLIENT_KEY = 495;
 
     extern const int KEEPER_EXCEPTION = 999;
     extern const int POCO_EXCEPTION = 1000;
