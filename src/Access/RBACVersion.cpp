275dd7cc8260bf7f5fa0281b2284fb7846544cfd
Vitaly Baranov <vitbar@yandex-team.ru>
2022-02-12 01:56:43 +0700

    SHOW CREATE ROW POLICY now always shows AS permissive/restrictive.

--- src/Access/RBACVersion.cpp
+++ src/Access/RBACVersion.cpp
@@ -3,8 +3,9 @@
 namespace DB
 {
 
-const UInt64 RBAC_INITIAL_VERSION = 1;
-const UInt64 RBAC_LATEST_VERSION = 1;
+extern const UInt64 RBAC_INITIAL_VERSION = 1;
+extern const UInt64 RBAC_LATEST_VERSION = 1;
+
 const char * RBAC_VERSION_SETTING_NAME = "rbac_version";
 
 }
