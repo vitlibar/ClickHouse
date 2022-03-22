db084531733c150d360a7e4db8acfef8c6871923
Vitaly Baranov <vitlibar@clickhouse.com>
2022-03-22 16:37:29 +0100

    Add new setting rbac_version.

---
+++ src/Access/RBACVersion.cpp
@@ -0,0 +1,10 @@
+#include <base/types.h>
+
+namespace DB
+{
+
+const UInt64 RBAC_INITIAL_VERSION = 1;
+const UInt64 RBAC_LATEST_VERSION = 1;
+const char * RBAC_VERSION_SETTING_NAME = "rbac_version";
+
+}
