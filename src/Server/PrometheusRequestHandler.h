901a3747afa3ebde2539039f4c27534c8325e2e0
Vitaly Baranov <vitlibar@clickhouse.com>
2024-05-14 17:08:14 +0200

    Refactor factory of PrometheusRequestHandler to make extending it easier.

--- src/Server/PrometheusRequestHandler.h
+++ src/Server/PrometheusRequestHandler.h
@@ -1,28 +1,30 @@
 #pragma once
 
 #include <Server/HTTP/HTTPRequestHandler.h>
+#include <Server/PrometheusRequestHandlerConfig.h>
 
-#include "PrometheusMetricsWriter.h"
 
 namespace DB
 {
-
 class IServer;
+class AsynchronousMetrics;
+class PrometheusMetricsWriter;
+using PrometheusMetricsWriterPtr = std::shared_ptr<const PrometheusMetricsWriter>;
 
 class PrometheusRequestHandler : public HTTPRequestHandler
 {
-private:
-    IServer & server;
-    PrometheusMetricsWriterPtr metrics_writer;
-
 public:
-    PrometheusRequestHandler(IServer & server_, PrometheusMetricsWriterPtr metrics_writer_)
-        : server(server_)
-        , metrics_writer(std::move(metrics_writer_))
-    {
-    }
+    PrometheusRequestHandler(IServer & server_, const PrometheusRequestHandlerConfig & config_, const AsynchronousMetrics & async_metrics_, const PrometheusMetricsWriterPtr & metrics_writer_);
+    ~PrometheusRequestHandler() override;
 
     void handleRequest(HTTPServerRequest & request, HTTPServerResponse & response, const ProfileEvents::Event & write_event) override;
+
+protected:
+    IServer & server;
+    const PrometheusRequestHandlerConfig config;
+    const AsynchronousMetrics & async_metrics;
+    const PrometheusMetricsWriterPtr metrics_writer;
+    const LoggerPtr log;
 };
 
 }
