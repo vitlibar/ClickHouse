ff2fa48806337a1e6c892bd075c93c717c512630
Vitaly Baranov <vitlibar@clickhouse.com>
2024-05-16 09:09:53 +0200

    Improve PrometheusBaseRequestHandler: send exception back to client, use ThreadStatus.

--- src/Server/PrometheusBaseRequestHandler.cpp
+++ src/Server/PrometheusBaseRequestHandler.cpp
@@ -1,11 +1,20 @@
 #include <Server/PrometheusBaseRequestHandler.h>
 
+#include <Common/ThreadStatus.h>
+#include <Common/setThreadName.h>
 #include <IO/HTTPCommon.h>
 #include <Server/HTTP/WriteBufferFromHTTPServerResponse.h>
+#include <Server/HTTP/exceptionCodeToHTTPStatus.h>
 
 
 namespace DB
 {
+
+namespace ErrorCodes
+{
+    extern const int HTTP_LENGTH_REQUIRED;
+}
+
 
 PrometheusBaseRequestHandler::PrometheusBaseRequestHandler(IServer & server_, const PrometheusRequestHandlerConfig & config_)
     : server(server_), config(config_), log(getLogger("PrometheusRequestHandler"))
@@ -16,8 +25,12 @@ PrometheusBaseRequestHandler::~PrometheusBaseRequestHandler() = default;
 
 void PrometheusBaseRequestHandler::handleRequest(HTTPServerRequest & request, HTTPServerResponse & response, const ProfileEvents::Event & write_event_)
 {
+    setThreadName("PrometheusHndlr");
+    ThreadStatus thread_status;
+
     try
     {
+        exception_is_written = false;
         write_event = write_event_;
         http_method = request.getMethod();
         chassert(!out);
@@ -28,7 +41,10 @@ void PrometheusBaseRequestHandler::handleRequest(HTTPServerRequest & request, HT
 
         setResponseDefaultHeaders(response, config.keep_alive_timeout);
 
-        handleMetrics(request, response);
+        if (request.getURI() == config.metrics.endpoint)
+            handleMetrics(request, response);
+        else
+            handlerNotFound(request, response);
 
         if (out)
         {
@@ -39,8 +55,18 @@ void PrometheusBaseRequestHandler::handleRequest(HTTPServerRequest & request, HT
     catch (...)
     {
         tryLogCurrentException(log);
+
+        ExecutionStatus status = ExecutionStatus::fromCurrentException("", /* with_stacktrace= */ false);
+        trySendExceptionToClient(status.message, status.code, request, response);
+
         out = nullptr;
     }
+}
+
+void PrometheusBaseRequestHandler::handlerNotFound(HTTPServerRequest & request, HTTPServerResponse & response)
+{
+    response.setStatusAndReason(Poco::Net::HTTPResponse::HTTP_NOT_FOUND);
+    writeString("There is no handler " + request.getURI() + "\n", getOutputStream(response));
 }
 
 WriteBuffer & PrometheusBaseRequestHandler::getOutputStream(HTTPServerResponse & response)
@@ -50,6 +76,54 @@ WriteBuffer & PrometheusBaseRequestHandler::getOutputStream(HTTPServerResponse &
     out = std::make_unique<WriteBufferFromHTTPServerResponse>(
         response, http_method == HTTPRequest::HTTP_HEAD, config.keep_alive_timeout, write_event);
     return *out;
+}
+
+void PrometheusBaseRequestHandler::trySendExceptionToClient(const String & s, int exception_code, HTTPServerRequest & request, HTTPServerResponse & response)
+{
+    try
+    {
+        if (!exception_is_written)
+        {
+            response.set("X-ClickHouse-Exception-Code", toString<int>(exception_code));
+            response.setStatusAndReason(exceptionCodeToHTTPStatus(exception_code));
+        }
+
+        /// If HTTP method is POST and Keep-Alive is turned on, we should read the whole request body
+        /// to avoid reading part of the current request body in the next request.
+        if (request.getMethod() == Poco::Net::HTTPRequest::HTTP_POST && response.getKeepAlive()
+            && exception_code != ErrorCodes::HTTP_LENGTH_REQUIRED && !request.getStream().eof())
+        {
+            request.getStream().ignoreAll();
+        }
+
+        if (!exception_is_written)
+        {
+            if (!out)
+            {
+                /// Nothing was sent yet.
+                getOutputStream(response);
+            }
+            else
+            {
+                /// Send the error message into already used stream.
+                /// Note that the error message will possibly be sent after some data.
+                /// Also HTTP code 200 could have already been sent.
+
+                /// If buffer has data, and that data wasn't sent yet, then no need to send that data
+                out->position() = out->buffer().begin();
+            }
+
+            writeString(s, *out);
+            writeChar('\n', *out);
+
+            out->finalize();
+            exception_is_written = true;
+        }
+    }
+    catch (...)
+    {
+        tryLogCurrentException(log, "Cannot send exception to client");
+    }
 }
 
 }
--- src/Server/PrometheusBaseRequestHandler.h
+++ src/Server/PrometheusBaseRequestHandler.h
@@ -20,10 +20,16 @@ public:
 
 protected:
     /// Writes the current metrics to the response in the Prometheus format.
-    virtual void handleMetrics(HTTPServerRequest & request, HTTPServerResponse & response) = 0;
+    virtual void handleMetrics(HTTPServerRequest & request, HTTPServerResponse & response) { handlerNotFound(request, response); }
+
+    /// Throws an exception that there is no handler for that path.
+    void handlerNotFound(HTTPServerRequest & request, HTTPServerResponse & response);
 
     /// Returns the write buffer used for the current HTTP response.
     WriteBuffer & getOutputStream(HTTPServerResponse & response);
+
+    /// Writes the current exception to the response.
+    void trySendExceptionToClient(const String & s, int exception_code, HTTPServerRequest & request, HTTPServerResponse & response);
 
     IServer & server;
     const PrometheusRequestHandlerConfig config;
@@ -33,6 +39,7 @@ protected:
 private:
     std::unique_ptr<WriteBufferFromHTTPServerResponse> out;
     ProfileEvents::Event write_event;
+    bool exception_is_written = false;
 };
 
 }
