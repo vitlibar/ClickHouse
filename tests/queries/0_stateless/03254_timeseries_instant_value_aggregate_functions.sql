9f312cb65fedf9409d852b944a6195e1fc462f4f
Vitaly Baranov <vitlibar@clickhouse.com>
2026-05-15 22:13:49 +0200

    Revert excessive setting change

--- tests/queries/0_stateless/03254_timeseries_instant_value_aggregate_functions.sql
+++ tests/queries/0_stateless/03254_timeseries_instant_value_aggregate_functions.sql
@@ -100,8 +100,8 @@ SELECT * FROM (
 ) ORDER BY name;
 
 
--- Tests to validate block header compatibility for serialized query plans
-SET serialize_query_plan=1, prefer_localhost_replica = false, enable_parallel_replicas = 0;
+-- Tests to validate block header compatibility in queries with parallel replicas
+SET serialize_query_plan=1, prefer_localhost_replica = false;
 
 SELECT
     metric_id,
