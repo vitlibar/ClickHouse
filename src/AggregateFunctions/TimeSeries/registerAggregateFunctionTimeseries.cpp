5c64924923274c77397d3c91179fbe4f1421a3a5
Vitaly Baranov <vitlibar@clickhouse.com>
2026-09-23 21:46:58 +0200

    My corrections

--- src/AggregateFunctions/TimeSeries/registerAggregateFunctionTimeseries.cpp
+++ src/AggregateFunctions/TimeSeries/registerAggregateFunctionTimeseries.cpp
@@ -16,7 +16,7 @@ void registerAggregateFunctionTimeseriesMax(AggregateFunctionFactory & factory);
 void registerAggregateFunctionTimeseriesMin(AggregateFunctionFactory & factory);
 void registerAggregateFunctionTimeseriesPresentToGrid(AggregateFunctionFactory & factory);
 void registerAggregateFunctionTimeseriesQuantileToGrid(AggregateFunctionFactory & factory);
-void registerAggregateFunctionTimeseriesVarianceOverTime(AggregateFunctionFactory & factory);
+void registerAggregateFunctionTimeseriesVariance(AggregateFunctionFactory & factory);
 void registerAggregateFunctionLast2Samples(AggregateFunctionFactory & factory);
 void registerAggregateFunctionTimeseriesGroupArray(AggregateFunctionFactory & factory);
 void registerAggregateFunctionTimeSeriesTopKMasks(AggregateFunctionFactory & factory);
@@ -36,7 +36,7 @@ void registerAggregateFunctionTimeseries(AggregateFunctionFactory & factory)
     registerAggregateFunctionTimeseriesMin(factory);
     registerAggregateFunctionTimeseriesPresentToGrid(factory);
     registerAggregateFunctionTimeseriesQuantileToGrid(factory);
-    registerAggregateFunctionTimeseriesVarianceOverTime(factory);
+    registerAggregateFunctionTimeseriesVariance(factory);
     registerAggregateFunctionLast2Samples(factory);
     registerAggregateFunctionTimeseriesGroupArray(factory);
     registerAggregateFunctionTimeSeriesTopKMasks(factory);
